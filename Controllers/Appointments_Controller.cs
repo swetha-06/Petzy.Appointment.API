5a1542ec23eae2c7224a2b87ebb7d428bed88184
swetha priyadharshini <swethapriyadharshini06@gmail.com>
2023-01-02 18:49:19 +0530

    Add project files.

---
+++ Controllers/Appointments_Controller.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Petzy.Appointment.DataAccess.RepositoryImpl;
+using Petzy.Appointment.Domain_.Repository;
+
+namespace Petzy.Appointment.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Appointments_Controller : ControllerBase
+    {
+        public IViewAppointmentRepository repository = new ViewAppointmentRepository();
+
+        [HttpGet]
+        public IActionResult GetAllAppointment() 
+        {
+            return Ok(repository.ViewAllAppointments());
+        }
+    }
+}
